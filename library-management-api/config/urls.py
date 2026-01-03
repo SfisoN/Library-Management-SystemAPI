4b702f1047cf295996a23e7ad126b14e845beb39
Sifiso Ntuli <sifisontulisn88@gmail.com>
2026-01-03 09:51:47 +0200

    IDK

--- library-management-api/config/urls.py
+++ library-management-api/config/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('users/', include('users.urls')),
-    path('/', include('checkouts.urls')),
+    path('', include('checkouts.urls')),
 
 
 
